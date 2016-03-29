3247c874c8a1e56ec48966b23e81f0d900671647
sheffcer <a.sheffcer@gmail.com>
2016-03-29 12:26:49 +0300

    поправил функцию проверки пользователя

--- task-1-2-js/js/script.js
+++ task-1-2-js/js/script.js
@@ -16,33 +16,59 @@ if ( n <= 0 ) {
   console.log(pow(x, n));
 }
 
+//
+//var userArray = [];
+//
+//function getUser() {
+//  for (var i =0; i < 3; i++) {
+//  userArray[i] = prompt('Введите пользователя №'+(i+1), '');
+// }
+//console.log(userArray);
+//}
+//
+//getUser();
+//
+//function checkUser () {
+// var  newUser=prompt('Введите имя пользователя', '');
+//
+//  if ( newUser.length < 1) {
+//    alert('Achtung partisanen!!!');
+//    return;
+//  }
+//  for ( i = 0; i < userArray.length; ++i) {
+//  if ( userArray[i] != newUser ) {
+//    alert(newUser+ 'Нет такого имени пользователя!');
+////    break;
+//  } else if(userArray[i] === newUser) {
+//    alert(newUser+ ', Вы удачно вошли');
+//    break;
+//  } 
+// }
+//}
+//checkUser();
 
 var userArray = [];
 
 function getUser() {
-  for (var i =0; i < 3; i++) {
-  userArray[i] = prompt('Введите пользователя №'+(i+1), '');
- }
-console.log(userArray);
+    for (var i =0; i < 5; i++) {
+        userArray[i] = prompt('Введите пользователя №'+(i+1), '');
+    }
+    console.log(userArray);
+}
+
+function checkUser () {
+    var  newUser=prompt('Введите имя пользователя', '');
+    if ( newUser.length < 1) {
+       return 'Achtung partisanen!!!';
+    }
+    for ( i = 0; i < userArray.length; ++i) {
+         if(userArray[i] === newUser) {
+            return newUser+ ', Вы удачно вошли';
+        }
+    }
+    return newUser+ ', Нет такого имени пользователя!';
 }
 
 getUser();
-
-function checkUser () {
- var  newUser=prompt('Введите имя пользователя', '');
-
-  if ( newUser.length < 1) {
-    alert('Achtung partisanen!!!');
-    return;
-  }
-  for ( i = 0; i < userArray.length; ++i) {
-  if ( userArray[i] != newUser ) {
-    alert(newUser+ 'Нет такого имени пользователя!');
-    break;
-  } else if(userArray[i] === newUser) {
-    alert(newUser+ ', Вы удачно вошли');
-    break;
-  } 
- }
-}
-checkUser();
+var message = checkUser();
+alert(message);
