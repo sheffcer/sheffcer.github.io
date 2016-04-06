74ae2ec57e3238db83c2599d5120ee86163910ad
sheffcer <a.sheffcer@gmail.com>
2016-04-06 16:30:08 +0300

    added object & title

--- task-3-4-js/js/main.js
+++ task-3-4-js/js/main.js
@@ -3,3 +3,15 @@ form.classList.add('navbar-form');
 var parentElem = document.body;
 //parentElem.insertBefore(form, parentElem.children[0]);
 parentElem.appendChild(form);
+
+var testForm =  {
+  Title: function() {
+    var name = 'Тест по программированию';
+    var pageTitle = document.createElement('h2'); 
+    pageTitle.classList.add('text-center');
+    pageTitle.innerHTML = name ;
+    form.insertBefore(pageTitle, form.children[0]);
+  }
+}
+
+testForm.Title(name);
