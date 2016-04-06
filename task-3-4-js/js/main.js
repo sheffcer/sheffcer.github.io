66a25986aaf4ad776b0a3116f2ad1925bf2bb685
sheffcer <a.sheffcer@gmail.com>
2016-04-06 15:29:50 +0300

    Подключение бустрапа создание тестовой страницы

--- task-3-4-js/js/main.js
+++ task-3-4-js/js/main.js
@@ -1,1 +1,15 @@
-alert('проверка подключения');
+var myForm = {
+  createBody: document.body,
+  createForm: document.createElement('form'),
+  createTitle: document.createElement('h2'),
+  createBtn: document.createElement(button),
+  
+  test: function () {
+    this.createForm.classList.add('navbar-form');
+    
+    this.createBody.appendChild(this.createForm);
+    this.createForm.appendChild(this.createTitle);
+  }
+}
+
+myForm.test();
