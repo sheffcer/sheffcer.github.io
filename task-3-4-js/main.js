6a5e923bb29629be0719bc1da05e321e11404c59
sheffcer <a.sheffcer@gmail.com>
2016-04-12 19:34:37 +0300

    fixed changes

---
+++ task-3-4-js/main.js
@@ -0,0 +1,34 @@
+//var obj = {
+//  0: '1',
+//  2: 'false'
+//}
+//
+//var student = {
+//  age: 18,
+//  name: 'Vasaya',
+//  second_name: 'Jorik'
+//}
+//
+////console.log(student.age);
+////console.log(student.name);
+//////var  key = 'second_name'  ;
+////
+//////console.log(student [key]);
+////
+////if ('second_name' in student) {
+////  console.log(student ['second_name']);
+////} else {
+////  console.log('no key')
+////}
+////  
+////
+////for (var key in student) {
+////  console.log('key', key);
+////  console.log('student[key]', student[key]);
+////  console.log('_____________');
+////}
+//
+//
+//var keylist = Object.keys(student);
+//console.log(keylist);
+
