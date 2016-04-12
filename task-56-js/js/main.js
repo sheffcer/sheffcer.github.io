9dd518f21b956f994036d4afa8284888a92a6685
sheffcer <a.sheffcer@gmail.com>
2016-04-12 17:48:27 +0300

    added new watcher code

--- task-56-js/js/main.js
+++ task-56-js/js/main.js
@@ -41,3 +41,64 @@ function increment() {
     }, 100)
   }
 }
+
+
+/*
+var timer = {
+startTime:0,
+out:document.getElementById('time'),
+addZeros:function (x,n) {
+    while (x.toString().length < n) {
+        x = "0" + x;
+    }
+    return x;
+},
+
+getCurrentTime:function(){
+ var d = new Date();
+ return d.getTime();
+},
+
+start:function(){
+var ct = this.getCurrentTime();
+this.startTime = ct;
+
+var that = this;
+ 
+setInterval(function(){
+ that.tick(that);
+ },1);
+},
+
+print:function(msg)
+{
+	this.out.innerHTML = msg;
+},
+
+msToTime: function (ms) {
+    var s  = this.addZeros(Math.floor((ms / 1000)  % 60), 2),
+        m  = this.addZeros(Math.floor((ms / 60000)  % 60), 2),
+        h    = this.addZeros(Math.floor((ms / 3600000) % 60), 2),
+        ms = this.addZeros(Math.floor(ms % 1000), 3);
+
+    return h + ':' + m + ':' + s + ':' + ms;
+  },
+
+tick:function(that) {
+
+var ct = that.getCurrentTime();
+var diff = ct - that.startTime;
+
+var sTime = that.msToTime(diff);
+
+that.out.innerHTML = sTime;
+
+}
+
+
+};
+
+
+timer.start();
+
+
