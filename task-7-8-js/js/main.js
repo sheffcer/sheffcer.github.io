bc30c2b134345650aca202ef4bde412f85df7aeb
sheffcer <a.sheffcer@gmail.com>
2016-04-12 11:25:15 +0300

    added tabs

---
+++ task-7-8-js/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $(".tabs-menu a").click(function(event) {
+        event.preventDefault();
+        $(this).parent().addClass("active");
+        $(this).parent().siblings().removeClass("active");
+        var tab = $(this).attr("href");
+        $(".tab-content").not(tab).css("display", "none");
+        $(tab).fadeIn();
+    });
+});
