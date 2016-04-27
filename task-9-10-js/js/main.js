6deb60ae416b1e1dd5fb01d0a5bd5f4025ea182c
sheffcer <a.sheffcer@gmail.com>
2016-04-27 14:24:31 +0300

    added dropdown-menu

--- task-9-10-js/js/main.js
+++ task-9-10-js/js/main.js
@@ -5,3 +5,16 @@ $(document).ready(function() {
 $(document).ready(function() {
   $("select").selecter();
 });
+
+//$(document).ready(function () {   
+//
+//    $('#menu ul li').hover(
+//        function () {
+//            $('ul:first', this).slideDown(150);  
+//        },
+//        function () {
+//            $('ul:first', this).slideUp(150);
+//        }
+//    ); 
+//	
+//});
