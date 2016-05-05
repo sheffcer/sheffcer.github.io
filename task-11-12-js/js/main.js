d9dfc4c89787ce345b3318baf156475132d35942
sheffcer <a.sheffcer@gmail.com>
2016-05-05 15:57:50 +0300

    editing template

--- task-11-12-js/js/main.js
+++ task-11-12-js/js/main.js
@@ -1,21 +1,57 @@
 $(function() {
   
-  var html = $('#test').html();
+//  var html = $('#test').html();
+//  
+//  var articles = [
+//    {
+//      title: 'Article 1',
+//      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, sint!'
+//    },
+//    
+//    {
+//      title: 'Article 2',
+//      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, sint!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, sint!'
+//    },
+//    
+//    {
+//      title: 'Article 3',
+//      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, sint!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, sint!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, sint!'
+//    }
+//  ]
+//      
+//  var content = tmpl(html, {
+//    data: articles
+//  });
+//  
+//  $('body').append(content);
+  
+  
+ var html = $('#test').html();
   
   var articles = [
     {
-      title: 'Article 1',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, sint!'
+      title: 'Шевко Александр Владимирович',
+      content: ['http://sheffcer.github.io/task-1_2/img/me.jpg']
     },
     
     {
-      title: 'Article 2',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, sint!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, sint!'
+      title: 'Администратор бд',
+      content: []
     },
     
     {
-      title: 'Article 3',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, sint!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, sint!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, sint!'
+      title: 'Хочу учить фронтенду потому что:',
+      content: ['Круто', 'Интересно', 'Востребовано']
+    },
+    
+    {
+      title: 'Контакты',
+      content: ['телефон: 050-442-2623', 'skype: sheffcer', 'почта: a.sheffcer@gmail.com']
+    },
+    
+    {
+      title: 'Хобби',
+      content: ['фото', 'коты', 'туризм']
     }
   ]
       
