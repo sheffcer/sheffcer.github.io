768efc010f5465c7ed23e25bf6442630c04121be
sheffcer <a.sheffcer@gmail.com>
2016-05-12 17:40:25 +0300

    added animals object

--- test/script.js
+++ test/script.js
@@ -1,14 +1,60 @@
-var animal = {
-  name:'jack',
-  color: 'black',
-  age: 5
-};
+//var animal = {
+//  name:'jack',
+//  color: 'black',
+//  age: 5
+//};        
+//
+////var str = JSON.stringify(animal);
+////
+////console.log(str);
+////
+////var obj = JSON.parse(str);
+////
+////console.log(obj);
+////console.log(obj.name, ',', obj.age, 'лет');
+//
+//
+//
+////console.log(JSON.stringify(animal));
+//
+//
+//localStorage.setItem('myAnimal', JSON.stringify(animal));
+//
+//var test = localStorage.getItem('myAnimal');
+//
+//test = JSON.parse(test);
+//console.log(test);
 
- var str = JSON.stringify(animal);
 
-console.log(str);
+'use strict';
+var questions = [
+  {
+    question_title: 'Какое самое популярное домашнее животное?',
+    answer: ['кот', 'медведь', 'лиса'],
+    true_answer: [true, false,false]
+  },
+  
+  {
+    question_title: 'Какие звуки издает лиса?',
+    answer: ['мяу', 'гав', 'никакие'],
+    true_answer: [false, false, true]
+  },
+  
+  {
+    question_title: 'Где живут коты?',
+    answer: ['нора', 'трава', 'гнездо'],
+    true_answer: [false, false, true]
+  }
+]
 
-var obj = JSON.parse(str);
+var JSON_Questions = JSON.stringify(questions); // упаковываем объект в строку
 
-console.log(obj);
-console.log(obj.color);
+console.log(JSON_Questions);
+
+localStorage.setItem('LS_questions', JSON_Questions); // записываем нашу строку в виде значени для ключа 'LS_questions'
+
+var storeQuestions = localStorage.getItem('LS_questions'); // получем нашу строку и localStorage
+
+storeQuestions = JSON.parse(storeQuestions); // преобразовываем нашу строку в объект
+
+console.log(storeQuestions);
