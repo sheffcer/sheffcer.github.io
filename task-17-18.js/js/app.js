ca407e86c2b584a5bb3b7e4a6e1d4a3d376df4da
sheffcer <a.sheffcer@gmail.com>
2016-06-03 14:56:26 +0300

    added bounce effect

--- task-17-18.js/js/app.js
+++ task-17-18.js/js/app.js
@@ -9,6 +9,24 @@
   moveSecondHands();
   // Set the intial minute hand container transition, and then each subsequent step
   setUpMinuteHands();
+
+  $( '.js-new-york' ).mouseenter(function() {
+    $( ".icon--usa" ).effect( "bounce", { times: 1 }, "slow" );
+  },null);
+
+  $( '.js-slovakia' ).mouseenter(function() {
+    $( ".icon--europe" ).effect( "bounce", { times: 1 }, "slow" );
+  },null);
+
+  $( '.js-kiev' ).mouseenter(function() {
+    $( ".icon--ukraine" ).effect( "bounce", { times: 1 }, "slow" );
+  },null);
+
+  $( '.js-hong-kong' ).mouseenter(function() {
+    $( ".icon--honkong" ).effect( "bounce", { times: 1 }, "slow" );
+  },null);
+
+
 })();
 
 /*
