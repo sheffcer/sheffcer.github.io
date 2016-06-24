13831bd5fbc9e1240b411dd439626c393a9e18b8
sheffcer <a.sheffcer@gmail.com>
2016-06-24 12:45:54 +0300

    added getLastBirthday func

--- task-17-18.js/js/test.js
+++ task-17-18.js/js/test.js
@@ -17,3 +17,27 @@
 
 
 })();
+
+// last birthday
+  function getLastBirthday() {
+  var dayNow = new Date();
+  var day = dayNow.getDate();
+  var month = dayNow.getMonth() + 1; // месяцы нумеруются с 0
+  var year = dayNow.getFullYear();
+
+  var Year;
+   if (month > 1 || (month === 1 && day > 2)) {
+//    if (month === 1 && day === 2) {
+      Year = year;
+    } else {
+       Year = year - 1;
+    }
+    return new Date(Year, 0, 2);
+   };
+
+var xxx = getLastBirthday();
+console.log(xxx);
+var now  = new Date();
+var dayPassed = (now -  getLastBirthday())/24/60/60/1000;
+console.log(dayPassed);
+
